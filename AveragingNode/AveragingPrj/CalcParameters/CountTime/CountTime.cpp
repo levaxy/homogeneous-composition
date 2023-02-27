9a9995f1669bc74ad7defda1fb027c9309d46326
lys9 <lys9@tpu.ru>
2023-02-27 21:55:18 +0700

    Create CountTime.cpp
    Вызов функции расчёта удельного расхода по выходу контейнера на пресс.

---
+++ AveragingNode/AveragingPrj/CalcParameters/CountTime/CountTime.cpp
@@ -0,0 +1,14 @@
+﻿#include"CountTime.h"
+
+void CalcParams(const int& TimeInProcess, const int& Condition){
+    int Q = 100, Mass = 60;
+    TotalTime += TimeInProcess;
+    CountContainers++;
+    if(Condition == 8)
+        CountGoodContainers++;
+    SpecificConsum = TotalTime*Q/(Mass*CountContainers);//общее время работы надо разделить на общую массу продукта, но как быть с браком? Если произведённым продуктом считать не брак и брак с любого этапа, то надо во всех гнёздах посчитать, сколько раз встречается подряд идущих 5/9/8
+
+    SpecificConsumGood = TotalTime*Q/(Mass*CountGoodContainers);// Тут вроде правильно, общее время работы делим на общую массу качественного порошка
+
+}
+
